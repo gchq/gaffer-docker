42ab3898490fcf942a1a0400431e761b222c6a60
d47853 <d47853@users.noreply.github.com>
2020-03-09 13:58:17 +0000

    addressed timing issue

--- cd/install.sh
+++ cd/install.sh
@@ -10,6 +10,12 @@ cd kubernetes/hdfs
 kind load docker-image gchq/hdfs:3.2.1
 helm install hdfs .
 
+# Wait for pod deployment
+while [[ $(kubectl get pods hdfs-datanode-2 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
+    echo "waiting for pod" 
+    && sleep 5; 
+done
+
 # Delete datanode to get around dodgy hdfs namenode issue
 kubectl delete pod hdfs-datanode-0 hdfs-datanode-1 hdfs-datanode-2
 
