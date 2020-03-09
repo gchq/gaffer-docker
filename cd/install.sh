6e392206bd7066d188ea8e0019c6046e3c7892bc
d47853 <d47853@users.noreply.github.com>
2020-03-09 15:28:14 +0000

    command mix up

--- cd/install.sh
+++ cd/install.sh
@@ -15,7 +15,7 @@ cd ../..
 # Deploy HDFS
 cd kubernetes/hdfs
 kind load docker-image gchq/hdfs:3.2.1
-helm install --wait hdfs .
+helm install hdfs . --wait
 
 # Delete datanode to get around dodgy hdfs namenode issue
 kubectl delete pod hdfs-datanode-0 hdfs-datanode-1 hdfs-datanode-2
