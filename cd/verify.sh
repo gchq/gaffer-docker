6fb29f5fcdae2eb835c489749dbc4d73f997fcd0
d47853 <d47853@users.noreply.github.com>
2020-03-09 17:04:16 +0000

    removed debugging lines from verify script

--- cd/verify.sh
+++ cd/verify.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-# set -e
-# if [ ${TRAVIS_PULL_REQUEST} != 'true' ]; then
-#     exit 0
-# fi
+set -e
+if [ ${TRAVIS_PULL_REQUEST} != 'true' ]; then
+    exit 0
+fi
 # Run tests
 cd kubernetes/hdfs
 helm test hdfs
-
-echo "auth test logs"
-kubectl logs hdfs-auth-test
-
-echo "hdfs datanode 0 logs"
-kubectl logs hdfs-datanode-0
-
-echo "hdfs datanode 1 logs"
-kubectl logs hdfs-datanode-1
-
-echo "hdfs datanode 2 logs"
-kubectl logs hdfs-datanode-2
