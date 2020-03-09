60bc8a4c1d6b46c44fe170bcce1572a135f4c059
d47853 <d47853@users.noreply.github.com>
2020-03-09 15:55:04 +0000

    debugging

--- cd/verify.sh
+++ cd/verify.sh
@@ -6,3 +6,16 @@ set -e
 # Run tests
 cd kubernetes/hdfs
 helm test hdfs
+
+echo "auth test logs"
+kubectl logs hdfs-auth-test
+
+echo "hdfs datanode 0 logs"
+kubectl logs hdfs-datanode-0
+
+echo "hdfs datanode 1 logs"
+kubectl logs hdfs-datanode-1
+
+
+echo "hdfs datanode 2 logs"
+kubectl logs hdfs-datanode-2
