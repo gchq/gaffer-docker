92e265940285191e134d726d5d7adf3bf9dfc292
d47853 <d47853@users.noreply.github.com>
2020-03-13 17:52:06 +0000

    added head filter

--- cd/verify.sh
+++ cd/verify.sh
@@ -15,7 +15,7 @@ for p in $(kubectl get pods --namespace=kube-system -l k8s-app=kube-dns -o name)
 
 echo "Doing DNS lookup"
 
-kubectl exec hdfs-namenode-0 -- bash -c "ip=\$(dig +short +search hdfs-datanode-0.hdfs-datanodes) && echo \${ip} && dig +search \${ip}"
+kubectl exec hdfs-namenode-0 -- bash -c "ip=\$(dig +short +search hdfs-datanode-0.hdfs-datanodes | head -1) && echo \${ip} && dig +search \${ip}"
 
 echo "Getting dns settings"
 
