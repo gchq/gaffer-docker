5c041d207ddc034d169a8538a3495a968049217e
d47853 <d47853@users.noreply.github.com>
2020-03-13 11:47:25 +0000

    debugging dns issue with nslookup

--- cd/verify.sh
+++ cd/verify.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
+# set -e
 # if [ ${TRAVIS_PULL_REQUEST} != 'true' ]; then
 #     exit 0
 # fi
 # Run tests
 cd kubernetes/hdfs
 helm test hdfs
+
+kubectl hdfs-namenode-0 -- nslookup hdfs-datanode-0
+kubectl hdfs-namenode-0 -- nslookup hdfs-datanode-0.hdfs-datanodes
