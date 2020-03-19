cf59a799b5f5191990ce59affb61933310e2c865
ctas582 <ctas582@users.noreply.github.com>
2020-03-19 18:07:51 +0000

    Wait for HDFS instances used by Accumulo to be ready before attempting to init the cluster

--- docker/accumulo/entrypoint.sh
+++ docker/accumulo/entrypoint.sh
@@ -3,7 +3,25 @@
 test -z "${ACCUMULO_CONF_DIR}" && HADOOP_CONF_DIR="${ACCUMULO_HOME}/conf"
 test -z "${ACCUMULO_INSTANCE_NAME}" && ACCUMULO_INSTANCE_NAME="accumulo"
 
+HADOOP_HOME=/opt/hadoop
+
 if [ "$1" = "accumulo" ] && [ "$2" = "master" ]; then
+	# If possible, wait until all the HDFS instances that Accumulo will be using are available i.e. not in Safe Mode
+	ACCUMULO_VOLUMES=$(xmlstarlet sel -t -v "/configuration/property[name='instance.volumes']/value" ${ACCUMULO_CONF_DIR}/accumulo-site.xml)
+	if [ ! -z "${ACCUMULO_VOLUMES}" ]; then
+		until [ "${ALL_VOLUMES_READY}" == "true" ] || [ $(( ATTEMPTS++ )) -gt 6 ]; do
+			echo "$(date) - Waiting for all HDFS instances to be ready..."
+			ALL_VOLUMES_READY="true"
+			for ACCUMULO_VOLUME in ${ACCUMULO_VOLUMES//,/ }; do
+				SAFE_MODE_CHECK=$(java -cp ${ACCUMULO_CONF_DIR}:${HADOOP_HOME}/share/hadoop/hdfs/*:${HADOOP_HOME}/share/hadoop/client/*:${HADOOP_HOME}/share/hadoop/common/lib/* org.apache.hadoop.hdfs.tools.DFSAdmin --fs ${ACCUMULO_VOLUME} -safemode get)
+				echo ${ACCUMULO_VOLUME} "-" ${SAFE_MODE_CHECK}
+				echo ${SAFE_MODE_CHECK} | grep -q "Safe mode is OFF" || ALL_VOLUMES_READY="false"
+			done
+			[ "${ALL_VOLUMES_READY}" == "true" ] || sleep 10
+		done
+		[ "${ALL_VOLUMES_READY}" == "true" ] || (echo "$(date) - ERROR: Timed out waiting for HDFS instances to be ready..." && exit 1)
+	fi
+
 	# Try to find desired root password from environment variable
 	PASSWORD="${ACCUMULO_ROOT_PASSWORD}"
 
