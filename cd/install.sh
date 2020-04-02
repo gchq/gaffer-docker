3e945225329a49e0f0cbdb19bd65314088530361
ctas582 <ctas582@users.noreply.github.com>
2020-04-02 13:13:53 +0000

    gh-25 Update CI/CD to deploy and test Road Traffic Helm Chart

--- cd/install.sh
+++ cd/install.sh
@@ -18,17 +18,18 @@ fi
 # Create a cluster 
 kind create cluster --quiet
 
-cd kubernetes/gaffer
+cd kubernetes/gaffer-road-traffic
 # Build images
 docker-compose --project-directory ../../docker/accumulo/ -f ../../docker/accumulo/docker-compose.yaml build
-docker-compose --project-directory ../../docker/gaffer/ -f ../../docker/gaffer/docker-compose.yaml build
+docker-compose --project-directory ../../docker/gaffer-road-traffic-loader/ -f ../../docker/gaffer-road-traffic-loader/docker-compose.yaml build
 
 # Deploy Images to Kind
 kind load docker-image gchq/hdfs:3.2.1
 kind load docker-image gchq/gaffer:1.11.0
 kind load docker-image gchq/gaffer-wildfly:1.11.0
+kind load docker-image gchq/gaffer-road-traffic-loader:1.11.0
 
 # Deploy containers onto Kind
 # Travis needs this setting to avoid reverse dns lookup errors
 echo "Starting helm install"
-helm install gaffer . -f ./values-insecure.yaml --set hdfs.config.hdfsSite."dfs\.namenode\.datanode\.registration\.ip-hostname-check"=false
+helm install gaffer . -f ./values-insecure.yaml --set gaffer.hdfs.config.hdfsSite."dfs\.namenode\.datanode\.registration\.ip-hostname-check"=false
