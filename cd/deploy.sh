aee0a44d9b155b2b29f7c9c3a9ae548453ef72a2
d47853 <d47853@users.noreply.github.com>
2020-04-08 14:22:33 +0100

    gh-22 applied PR review fixes

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 # Gets project root directory by calling two nested "dirname" commands on the this file 
 getRootDirectory() {
-    echo "$( cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" > /dev/null 2>&1 && pwd )"
+    echo "$( cd $(dirname $(dirname $0)) > /dev/null 2>&1 && pwd )"
 }
 
 # Pushes Tags to Dockerhub
@@ -12,21 +12,19 @@ pushTags() {
     name=$1
     version=$2
     app_version=$3
-
-    tags="$(echo "${version}" | sed -e "s/\(.*\)\.\(.*\)\..*/"${name}":"${version}"+"${app_version}" "${name}":"${version}" "${name}":\1.\2 "${name}":\1 "${name}":latest/")"
-
+    tags="$(echo ${version} | sed -e "s|\(.*\)\.\(.*\)\..*|${name}:${version}+${app_version} ${name}:${version} ${name}:\1.\2 ${name}:\1 ${name}:latest|")"
     docker tag "${name}" "${tags}"
     docker push "${tags}"
 }
 
 # If branch is not master or is pull request, exit
-if [ "${TRAVIS_PULL_REQUEST}" != "false"] || [ "${TRAVIS_BRANCH}" != "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
     exit 0
 fi
 
 # Retrieve versions from files
 ROOT_DIR="$(getRootDirectory)"
-APP_VERSION="$(cat "${ROOT_DIR}"/app_version)"
+APP_VERSION="$(cat ${ROOT_DIR}/app_version)"
 
 # This set's the values for:
 # HADOOP_VERSION
@@ -46,7 +44,7 @@ pushTags gchq/gaffer-road-traffic-loader "${GAFFER_VERSION}" "${APP_VERSION}"
 
 # Setup Git Credentials
 git config --global credential.helper "store --file=.git/credentials"
-echo "https://"${GITHUB_TOKEN}":@github.com" > .git/credentials
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 
 # Add Develop branch
 git remote set-branches --add origin develop
@@ -60,9 +58,9 @@ git push origin "${TAG_NAME}"
 # Create release notes
 REPO_NAME="Gaffer-Docker"
 JSON_DATA="{
-                \"tag_name\": \""${TAG_NAME}"\",
-                \"name\": \""${REPO_NAME}" "${APP_VERSION}"\",
-                \"body\": \"["${APP_VERSION}" headliners](https://github.com/gchq/"${REPO_NAME}"/issues?q=milestone%3A"${TAG_NAME}"+label%3Aheadliner)\n\n["${APP_VERSION}" enhancements](https://github.com/gchq/"${REPO_NAME}"/issues?q=milestone%3A"${TAG_NAME}"+label%3Aenhancement)\n\n["${APP_VERSION}" bugs fixed](https://github.com/gchq/"${REPO_NAME}"/issues?q=milestone%3A"${TAG_NAME}"+label%3Abug)\n\n["${APP_VERSION}" migration notes](https://github.com/gchq/"${REPO_NAME}"/issues?q=milestone%3A"${TAG_NAME}"+label%3Amigration-required)\n\n["${APP_VERSION}" all issues resolved](https://github.com/gchq/"${REPO_NAME}"/issues?q=milestone%3A"${TAG_NAME}")\",
+                \"tag_name\": \"${TAG_NAME}\",
+                \"name\": \"${REPO_NAME} ${APP_VERSION}\",
+                \"body\": \"[${APP_VERSION} headliners](https://github.com/gchq/${REPO_NAME}/issues?q=milestone%3A${TAG_NAME}+label%3Aheadliner)\n\n[${APP_VERSION} enhancements](https://github.com/gchq/${REPO_NAME}/issues?q=milestone%3A${TAG_NAME}+label%3Aenhancement)\n\n[${APP_VERSION} bugs fixed](https://github.com/gchq/${REPO_NAME}/issues?q=milestone%3A${TAG_NAME}+label%3Abug)\n\n[${APP_VERSION} migration notes](https://github.com/gchq/${REPO_NAME}/issues?q=milestone%3A${TAG_NAME}+label%3Amigration-required)\n\n[${APP_VERSION} all issues resolved](https://github.com/gchq/${REPO_NAME}/issues?q=milestone%3A${TAG_NAME})\",
                 \"draft\": false
             }"
 echo "${JSON_DATA}"
--- cd/update_app_version.sh
+++ cd/update_app_version.sh
@@ -4,13 +4,13 @@ set -e
 
 # Gets project root directory by calling two nested "dirname" commands on the this file 
 getRootDirectory() {
-    echo "$( cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" > /dev/null 2>&1 && pwd )"
+    echo "$( cd $(dirname $(dirname $0)) > /dev/null 2>&1 && pwd )"
 }
 
-# Get version in app.version
+# Get version in app_version
 getAppVersion() {
     root_dir="$(getRootDirectory)"
-    echo "$(cat "$root_dir"/app_version)"
+    echo "$(cat ${root_dir}/app_version)"
 }
 
 # Get Command name ("update_app_version.sh")
@@ -20,16 +20,16 @@ getCommand() {
 
 # Increments the bugfix version by 1
 createNewVersion() {
-    old_version=$1
-    major_and_minor="$(echo "${old_version}" | sed 's|^\(.*\.\).*|\1|')"
-    bugfix="$(echo "${old_version}" | sed 's|.*\.||')"
-    new_bugfix="$(("${bugfix}" + 1))"
-    echo "${major_and_minor}""${new_bugfix}"
+    old_version="$1"
+    major_and_minor="$(echo ${old_version} | sed 's|^\(.*\.\).*|\1|')"
+    bugfix="$(echo ${old_version} | sed 's|.*\.||')"
+    let "bugfix++"
+    echo "${major_and_minor}""${bugfix}"
 }
 
-# Performs a Find and replace on the Helm charts and the app.version file
+# Performs a Find and replace on the Helm charts and the app_version file
 findAndReplace() {
-    find "$(getRootDirectory)" \( -iname Chart.y*ml -o -name app.version \) -exec sed -i '' -e "s:"$1":"$2":g" {} +
+    find "$(getRootDirectory)" \( -iname Chart.y*ml -o -name app_version \) -exec sed -i'' -e "s:$1:$2:g" {} +
 }
 
 if [ $# -gt 1 ]; then
@@ -44,7 +44,7 @@ new_version=$1
 app_version="$(getAppVersion)"
 
 if [ -z "${new_version}" ]; then
-    new_version="$(createNewVersion "${app_version}")"
+    new_version="$(createNewVersion ${app_version})"
 fi
 
 findAndReplace "${app_version}" "${new_version}"
