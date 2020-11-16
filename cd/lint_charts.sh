b5a73f2754da851410fa0551bbb0f8ea9814d596
d47853 <d47853@users.noreply.github.com>
2020-11-16 12:02:02 +0000

    gh-110 updated zookeeper repo

--- cd/lint_charts.sh
+++ cd/lint_charts.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 project_root="$( cd $(dirname $(dirname $0)) > /dev/null 2>&1 && pwd )"
 for chart in ${project_root}/kubernetes/*; do	
     if [ -f "${chart}/Chart.yaml" ]; then	
