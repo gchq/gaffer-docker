05392e77256a386012c02752cd55037190a9abeb
p3430233 <p3430233@outlook.com>
2021-02-10 03:48:36 -0800

    wip

--- docker/gaffer-integration-tests/docker-entrypoint.sh
+++ docker/gaffer-integration-tests/docker-entrypoint.sh
@@ -22,18 +22,21 @@ accumulo_instance=$(cat conf/store.properties | grep accumulo.instance | sed -e 
 accumulo_zookeepers=$(cat conf/store.properties | grep accumulo.zookeepers | sed -e 's/.*=\(.*\)/\1/')
 accumulo_user=$(cat conf/store.properties | grep accumulo.user | sed -e 's/.*=\(.*\)/\1/')
 accumulo_password=$(cat conf/store.properties | grep accumulo.password | sed -e 's/.*=\(.*\)/\1/')
-store_properties=$(find /tmp/gaffer/store-implementation/accumulo-store/src/test/resources -name *.properties)
+store_properties=$(find /tmp/gaffer/store-implementation/accumulo-store/src/test/resources -name *.properties | grep -v cache)
 
 
 for store in $store_properties; do
+echo $store
 sed -i'' -e "s/gaffer.store.class=.*/gaffer.store.class=$store_class/" $store
 sed -i'' -e "s/accumulo.instance=.*/accumulo.instance=$accumulo_instance/" $store
 sed -i'' -e "s/accumulo.zookeepers=.*/accumulo.zookeepers=$accumulo_zookeepers/" $store
 sed -i'' -e "s/accumulo.user=.*/accumulo.user=$accumulo_user/" $store
 sed -i'' -e "s/accumulo.password=.*/accumulo.password=$accumulo_password/" $store
+cat $store
 done
+
 # Run Integration Tests 
 cd /tmp/gaffer
 mvn -q clean install -pl :accumulo-store -am -Pquick
-mvn -q verify -pl :accumulo-store -ff
+mvn verify -pl :accumulo-store -ff
 
