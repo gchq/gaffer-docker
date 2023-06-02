8a52447574abdcea2889824411d6776dd1a55f04
t92549 <80890692+t92549@users.noreply.github.com>
2023-06-02 15:21:21 +0100

    Gh-314: Remove mentions of v2-alpha (#315)
    * Remove mentions of v2-alpha
    
    * Copyright
    
    * Fix git user

--- cd/fix_copyright_headers.sh
+++ cd/fix_copyright_headers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2022 Crown Copyright
+# Copyright 2022-2023 Crown Copyright
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ then
 fi
 
 currentYear=$(date +%Y)
-diff=`git diff origin/v2-alpha --name-only --cached`
+diff=`git diff origin/develop --name-only --cached`
 
 if [ -n "$diff" ]
 then
